497012feda8113cda254fc783381bf6a978af8a9
Andreas Karrenbauer <a.karrenbauer@asw-ggmbh.de>
2025-09-23 11:26:10 +0200

    Print token

--- machine/CompilerParser.java
+++ machine/CompilerParser.java
@@ -19,7 +19,7 @@ public class CompilerParser implements CompilerParserConstants {
 
     public static void main(String[] args) throws ParseException {
         CompilerParser parser = new CompilerParser(System.in);
-        parser.Program(false);
+        parser.Program();
     }
 
   final public List<Number> Program(boolean preflight) throws ParseException {
@@ -378,7 +378,7 @@ if( !hasElse ) {
     }
     jj_consume_token(29);
 if( !preflight ) {
-            {if (true) throw new ParseException( "Macro substitution failed!");}
+            {if (true) throw new ParseException( "Macro substitution failed!" + token.image);}
         }
 }
 
